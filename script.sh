08e2b8bb594169b1bd2e234ea1368eeb6aa19aa8
Oliver Cutting <oliver.cutting@gmail.com>
2022-09-14 20:36:37 +0100

    Added Docker Desktop

--- script.sh
+++ script.sh
@@ -30,11 +30,16 @@ wget https://raw.githubusercontent.com/daltonmenezes/aura-theme/main/packages/gn
 dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ~/aura-theme.dconf
 rm ~/aura-theme.dconf
 
-# Install Docker
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# Install Docker & Docker Desktop
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt install docker-ce docker-ce-cli containerd.io uidmap
+sudo usermod -aG docker $USER
+newgrp docker
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.12.0-amd64.deb
+sudo apt install ./docker-desktop-*-amd64.deb
+rm docker-desktop-*-amd64.deb
+
+# Install Helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
