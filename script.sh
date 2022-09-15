184ceaa6823d0214f028c7f0a95deb42c787742e
Oliver Cutting <oliver.cutting@gmail.com>
2022-09-15 09:59:09 +0100

    Added AWSCLI, Kubectl, EKSCTL

--- script.sh
+++ script.sh
@@ -43,3 +43,15 @@ rm docker-desktop-*-amd64.deb
 
 # Install Helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Install AWSCLI
+sudo apt  install awscli
+
+# Install Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+# Install EKSctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
