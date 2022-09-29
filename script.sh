0d1d295804a2fa8d6ffe883f06ad498dd19e2e94
Oliver Cutting <oliver.cutting@gmail.com>
2022-09-29 13:25:33 +0100

    Removed broken installs

--- script.sh
+++ script.sh
@@ -2,10 +2,6 @@
 sudo sed -i 's/load-module module-suspend-on-idle/#load-module module-suspend-on-idle/' /etc/pulse/default.pa
 systemctl restart --user pulseaudio
 
-# Install Git
-sudo apt update
-sudo apt install git
-
 # Install Vim
 sudo apt install vim
 
@@ -21,16 +17,11 @@ sudo apt install curl
 # Install Gnome Tweak Tool
 sudo apt install gnome-tweaks
 
-# Set wallpaper
-wget https://unsplash.com/photos/9wg5jCEPBsw/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjYzMTc4Nzc1&force=true&w=2400
-mv download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjYzMTc4Nzc1 ~/Pictures/mountains.jpg
-rm wget-log.1
-gsettings set org.gnome.desktop.background picture-uri-dark file:///home/oliver/Pictures/mountains.jpg
-gsettings set org.gnome.desktop.background picture-options stretched
+# Install Gnome Extensions
+sudo apt install gnome-shell-extensions
 
 # Install theme
-mkdir Projects
-cd Projects
+cd ~/Projects
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 cd WhiteSur-gtk-theme
 ./install.sh
@@ -44,22 +35,12 @@ wget https://raw.githubusercontent.com/daltonmenezes/aura-theme/main/packages/gn
 dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ~/aura-theme.dconf
 rm ~/aura-theme.dconf
 
-# Install Docker & Docker Desktop
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt install docker-ce docker-ce-cli containerd.io uidmap
-sudo usermod -aG docker $USER
-newgrp docker
-wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.12.0-amd64.deb
-sudo apt install ./docker-desktop-*-amd64.deb
-rm docker-desktop-*-amd64.deb
 
 # Install Helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 # Install awscli
-sudo apt  install awscli
+sudo apt install awscli
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
